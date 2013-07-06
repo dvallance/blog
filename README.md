9860af63d79b8bca2d93e2581592faed8549d476
David Vallance <david@maci.(none)>
2013-07-05 21:49:21 -0700

    Quickly moving my static blog to a sinatra app to run on my new domain.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Just quickly moving my old blog code to my new domain.
+
+Don't use this code as a good way of creating a Sinatra app, its just a really quick migration of a static blog for putting it on my new domain.
---
+++ wisethinker.rb
@@ -0,0 +1,30 @@
+require "rubygems"
+require "sinatra/base"
+
+class WiseThinker < Sinatra::Base
+	set :root, File.dirname(__FILE__)
+
+	get '/' do
+		erb :'/index.html', :layout => :'layouts/default.html'
+	end
+
+	get '/blog/getting-started-with-jeykll/?' do
+		erb :'/blog/getting-started-with-jeykll/index.html', :layout => :'layouts/default.html'
+	end
+
+	get '/blog/jeykll-how-post-naming-works/?' do
+		erb :'/blog/jeykll-how-post-naming-works/index.html', :layout => :'layouts/default.html'
+	end
+
+	get '/blog/part-one-walkthrough-how-to-build-a-modular-web-application-with-ruby/?' do
+		erb :'/blog/part-one-walkthrough-how-to-build-a-modular-web-application-with-ruby/index.html', :layout => :'layouts/default.html'
+	end
+	
+	get '/blog/walkthrough-how-to-build-a-modular-web-application-intro/?' do
+		erb :'blog/walkthrough-how-to-build-a-modular-web-application-intro/index.html', :layout => :'layouts/default.html'
+	end
+
+	get '/blog/installing-an-operating-system-on-a-zotac-nano-ad10/?' do
+		erb :'/blog/installing-an-operating-system-on-a-zotac-nano-ad10/index.html', :layout => :'layouts/default.html'
+	end
+end
